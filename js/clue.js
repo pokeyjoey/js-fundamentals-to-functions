6d2fa66c3ab0d65ac4c632abc8b60550dae79979
JR Jeff Roche (####) <e0054182@agiml-a08761.home>
2022-10-05 09:31:48 -0400

    Commit through destructuring

---
+++ js/clue.js
@@ -0,0 +1,29 @@
+var clue = {
+    characters: [
+        'Miss Scarlet',
+        'Professor Plum',
+        'Mrs Peacock',
+        'Reverend Green',
+        'Colonel Mustard',
+        'Dr Orchid'
+    ],
+    weapons: [
+        'Revolver',
+        'Dagger',
+        'Lead Pipe',
+        'Rope',
+        'Candlestick',
+        'Wrench'
+     ],
+     Rooms: [
+        'Hall',
+        'Kitchen',
+        'Lounge',
+        'Dining Room',
+        'Ballroom',
+        'Conservatory',
+        'Billard Room',
+        'Library',
+        'Study'
+    ]
+};
